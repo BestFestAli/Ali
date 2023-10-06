5fcdc089bbb6ac089b7f81bdd847c9b0d6b3d789
Ali Mustafa <bestfestali@gmail.com>
2023-10-07 02:01:32 +0600

    glava3.2

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,17 +1,24 @@
 package main
 
 import (
-	"errors"
-	"github.com/julienschmidt/httprouter"
+	"encoding/json"
 	"net/http"
-	"strconv"
 )
 
-func (app *application) readIDParam(r *http.Request) (int64, error) {
-	params := httprouter.ParamsFromContext(r.Context())
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
-	if err != nil || id < 1 {
-		return 0, errors.New("invalid id parameter")
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
 	}
-	return id, nil
+
+	js = append(js, '\n')
+
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+	return nil
 }
